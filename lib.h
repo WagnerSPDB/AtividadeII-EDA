505ab760277ccf5ddccf067061850af14a9bcd60
WagnerSPDB <114710587+WagnerSPDB@users.noreply.github.com>
2023-05-25 00:47:27 -0300

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,46 +1,57 @@
-#define MAX 13
-#include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
-#include <stddef.h>
-typedef struct {
-	int chave;
-	int valor;
-} Telemento;
-
+#include <stdio.h>
+#include <string.h>
+#define MAX 13
+//Tipo pessoa
 typedef struct {
     int numUnico, dataMes, dataAno;
     char nome [100];
 } Cartao;
 
+typedef struct no{
+    Cartao cartao;  //objeto cartao
+    struct no *proximo;  //Ponteiro para próximo nó
+} Cno;
+
 typedef struct{
-    char cpf [11], nome[100], endereco[200];
-    Cartao cartoes[10];
+    Cno *inicio;
+    int tam;
+} Clista;
+
+typedef struct{
+    int cpf;
+    char nome [100], endereco[200];
+    Cartao* cartoes[10];
+    int tam;
 } Usuario;
 
-typedef struct {
-    char descricao [200];
-    float valor;
+typedef struct uno{
     Usuario usuario;
-} Compra;
-
-
-struct no{
-        Cartao cartao;
-        struct no *proximo;
-};
-
-typedef struct no* Tno;
-
-
+    struct uno *proximo;
+} Uno;
 
 typedef struct{
-        Tno elemento[MAX];
-        int n;
-} TLista;
+    Uno *inicio;
+    int tam;
+} Ulista;
 
-int criar(TLista *L);
-int inserir(TLista *L, int hash, Cartao cartao);
-void exibir(TLista L);
-int hashing(int chave);
 
+Clista* criarListaC();
+Ulista* criarListaU();
+void inicializarC();
+void inicializarU();
+Cartao criarCartao();
+Usuario criarUsuario();
+int hashing(int num);
+void inserirInicioC(Cartao c, Clista *lista);
+void inserirInicioU(Usuario u, Ulista *lista);
+void inserirTabelaC();
+void inserirTabelaU();
+Cno* buscarNo(int num, Cno *inicio);
+Cartao* buscarCartaoTabela(int num);
+void imprimirCartao(Cartao c);
+void imprimirListaC(Cno *inicio);
+void imprimirTabelaC();
+void imprimirUsuario(Usuario u);
+void imprimirListaU(Uno *inicio);
+void imprimirTabelaU();
