b522cb4d597c8eef84787dbe7161a09aaee2548f
WagnerSPDB <114710587+WagnerSPDB@users.noreply.github.com>
2023-05-26 01:20:23 -0300

    Update main.c

--- main.c
+++ main.c
@@ -8,10 +8,13 @@ int main() {
     inicializarC();
     Usuario *u;
     inicializarU();
+    Compra *a = NULL;
+    Compra *aux = NULL;
+    DadoCompra dado;
 
     do{
-        printf("\t\t\tEditor de listas\n\t0 - Sair\n\t1 - Inserir novo cartao\n\t2 - Inserir novo usuario\n\t3 - Buscar cartao\n\t"
-               "4 - Imprimir tabela de cartoes\n\t5 - Imprimir tabela de usuarios\n");
+        printf("\t\t\tEditor de listas\n\t0 - Sair\n\t1 - Inserir novo cartao\n\t2 - Inserir novo usuario\n\t"
+               "3 - Inserir nova compra\n\t4 - Buscar cartao\n\t5 - Imprimir tabela de cartoes\n\t6 - Imprimir tabela de usuarios\n");
         scanf("%d", &opcao);
         switch(opcao){
         case 0:
@@ -24,6 +27,24 @@ int main() {
             inserirTabelaU();
             break;
         case 3:
+            printf("Informe o valor da compra: ");
+            scanf("%f", &dado.valor);
+            printf("Informe os itens da compra: ");
+            scanf("%*c");
+            fgets(dado.descricao, 200, stdin);
+            printf("Informe o CPF do usuario que realizou a compra: ");
+            scanf("%d", &num);
+            aux = buscarUsuarioTabela(num);
+            if(aux != NULL){
+                dado.usuario = aux;
+               // a->id++;
+            }else{
+                printf("Usuario inexistente!");
+                return;
+            }
+            a = inserirCompra(a, dado);
+            break;
+        case 4:
             printf("Qual o numero do cartao a ser buscado? ");
             scanf("%d", &num);
             c = buscarCartaoTabela(num);
@@ -34,11 +55,14 @@ int main() {
             else
                 printf("Cartao nao encontrada!\n");
             break;
-        case 4:
+        case 5:
             imprimirTabelaC();
             break;
-        case 5:
+        case 6:
             imprimirTabelaU();
+            break;
+        case 7:
+            imprime(a);
             break;
         default:
             printf("Opção Invilida!\n");
