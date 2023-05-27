87cc299192f140faa075bc02e2cfe3cc44132105
WagnerSPDB <114710587+WagnerSPDB@users.noreply.github.com>
2023-05-27 10:59:40 -0300

    Update lib.h

--- lib.h
+++ lib.h
@@ -3,10 +3,27 @@
 #include <string.h>
 #define MAX 13
 //Tipo pessoa
+typedef struct cmno Compra;
+
 typedef struct {
     int numUnico, dataMes, dataAno;
     char nome [100];
+    Compra *a;
 } Cartao;
+
+typedef struct{
+    int cpf;
+    char nome [100], endereco[200];
+    Cartao* cartoes[10];
+    int tam;
+} Usuario;
+
+typedef struct{
+    int id;
+    float valor;
+    char descricao[200];
+    Usuario* usuario;
+} DadoCompra;
 
 typedef struct no{
     Cartao cartao;  //objeto cartao
@@ -18,13 +35,6 @@ typedef struct{
     int tam;
 } Clista;
 
-typedef struct{
-    int cpf;
-    char nome [100], endereco[200];
-    Cartao* cartoes[10];
-    int tam;
-} Usuario;
-
 typedef struct uno{
     Usuario usuario;
     struct uno *proximo;
@@ -35,19 +45,14 @@ typedef struct{
     int tam;
 } Ulista;
 
-typedef struct{
-    int id;
-    float valor;
-    char descricao[200];
-    Usuario* usuario;
-} DadoCompra;
-
 typedef struct cmno{
 	DadoCompra dado;
 	int fator_b;
 	struct cmno* esq;
 	struct cmno* dir;
 } Compra;
+
+
 
 
 Clista* criarListaC();
@@ -61,6 +66,8 @@ void inserirInicioC(Cartao c, Clista *lista);
 void inserirInicioU(Usuario u, Ulista *lista);
 void inserirTabelaC();
 void inserirTabelaU();
+Compra* criarCompraNo(DadoCompra dado);
+Compra* criarCompra();
 Compra* inserirCompra(Compra* a, DadoCompra dado);
 Cno* buscarNo(int num, Cno *inicio);
 Uno* buscarNoUsu(int num, Uno *inicio);
@@ -72,4 +79,12 @@ void imprimirTabelaC();
 void imprimirUsuario(Usuario u);
 void imprimirListaU(Uno *inicio);
 void imprimirTabelaU();
+void imprime(Compra* a);
 void percorrerEmOrdem(Compra* raiz);
+int altura_AVL(Compra* a);
+int calcula_FB(Compra* a);
+Compra* rotacao_simples_esquerda(Compra* a);
+Compra* rotacao_simples_direita(Compra* a);
+Compra* balanceamento(Compra* a);
+Compra *balanceio_direita(Compra *a);
+Compra *balanceio_esquerda(Compra *a);
